1759f39c35ce5a5fb87deba6becce6f104162af7
Wenxuan Zhang <wenxuangm@gmail.com>
2023-12-08 00:39:25 +0800

    day03: add solution for part 2
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{solution, AocResult};
 
 struct Grid<'a> {
@@ -35,7 +37,41 @@ impl<'a> Grid<'a> {
 
     fn adjacent_to_symbol(&self, row: usize, col: usize) -> bool {
         ADJS.iter()
-            .any(|&(r, c)| self.is_symbol(row.wrapping_add_signed(r), col.wrapping_add_signed(c)))
+            .any(|&(i, j)| self.is_symbol(row.wrapping_add_signed(i), col.wrapping_add_signed(j)))
+    }
+
+    fn num_at(&self, row: usize, col: usize) -> Option<(usize, usize)> {
+        if row >= self.nrows || col >= self.ncols || !self.data[row][col].is_ascii_digit() {
+            return None;
+        }
+
+        let num_start = match self.data[row][..col].iter().rev().position(|c| !c.is_ascii_digit()) {
+            Some(n) => col - n,
+            None => 0,
+        };
+
+        let num = self.data[row][num_start..]
+            .iter()
+            .take_while(|c| c.is_ascii_digit())
+            .fold(0, |acc, &c| acc * 10 + (c - b'0') as usize);
+
+        Some((row * self.ncols + num_start, num))
+    }
+
+    fn gear_ratio(&self, row: usize, col: usize) -> Option<usize> {
+        if row >= self.nrows || col >= self.ncols || self.data[row][col] != b'*' {
+            return None;
+        }
+
+        let nums: HashMap<usize, usize> = ADJS
+            .iter()
+            .filter_map(|&(i, j)| self.num_at(row.wrapping_add_signed(i), col.wrapping_add_signed(j)))
+            .collect();
+
+        match nums.len() {
+            2 => Some(nums.values().product()),
+            _ => None,
+        }
     }
 }
 
@@ -68,15 +104,22 @@ fn part1(input: &str) -> AocResult<usize> {
     Ok(sum)
 }
 
-fn part2(_input: &str) -> AocResult<usize> {
-    todo!()
+fn part2(input: &str) -> AocResult<usize> {
+    let grid = Grid::new(input)?;
+
+    let sum = (0..grid.nrows)
+        .flat_map(|i| (0..grid.ncols).map(move |j| (i, j)))
+        .filter_map(|(i, j)| grid.gear_ratio(i, j))
+        .sum();
+
+    Ok(sum)
 }
 
-solution!(part1 => 529618, part2 => todo!());
+solution!(part1 => 529618, part2 => 77509019);
 
 #[cfg(test)]
 mod tests {
-    crate::test!(part1, t1: "
+    const EXAMPLE: &str = "
 467..114..
 ...*......
 ..35..633.
@@ -87,7 +130,9 @@ mod tests {
 ......755.
 ...$.*....
 .664.598..
-    ".trim() => 4361);
+";
 
-    crate::test!(part2, t1: "" => 42);
+    crate::test!(part1, t1: EXAMPLE.trim() => 4361);
+
+    crate::test!(part2, t1: EXAMPLE.trim() => 467835);
 }
